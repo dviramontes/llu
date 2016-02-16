96b133fd5b4ae3df0f088738ee8a81c19731434d
dviramontes <dviramontes@gmail.com>
2016-02-16 01:59:39 -0700

    adds Riff B

--- llu.1.cc-intimate.rb
+++ llu.1.cc-intimate.rb
@@ -2,28 +2,20 @@
 # B|  4|-----------G-----------G----------G----------G---|-
 
 notes1 = (ring :f5, :Cs5, :c5, :Gs4)
-notes2 = (ring :f5, :Cs5, :c5, :Gs4)
+notes2 = (ring :f5, :Gs5,:Cs5, :f5, :c5, :Ds4,:Gs4, :C4)
 
 use_bpm 125
 use_synth :pulse
 
-live_loop :beat do
+16.times do
   with_fx(:reverb, reps: 2, delay: 1, decay: 1, distort: 2.4,
   phase: 0.25) do
     sample :drum_bass_hard, release: 1.25
   end
-  sleep 1
-end
-
-live_loop :melody do
   with_fx(:reverb, decay: 3) do
     with_fx(:echo, release: 2.2,  phase: 0.125) do
-      play notes1.tick
-      sleep 1
+      play notes2 .tick
     end
   end
+  sleep 0.5
 end
-
-
-
-
