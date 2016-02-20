ee63c777d0fd8906d846dfcb66883ac1d89b90aa
dviramontes <dviramontes@gmail.com>
2016-02-20 14:19:14 -0700

    + keyframes

--- vidseq/src/cljs/vidseq/app.cljs
+++ vidseq/src/cljs/vidseq/app.cljs
@@ -6,9 +6,6 @@
 
 (def duration (r/atom 0))
 
-(def query (r/atom {:query  ""
-                    :offset 0}))
-
 (def source-url (r/atom ""))
 
 (def voice-1 (r/atom "My first show show"))
@@ -40,26 +37,39 @@
                      8 (refresh! "robot+love" 5 "of a robot")
                      12 (refresh! "robot+walking" nil "who falls in love")
                      16 (refresh! "black+friday" 1 "with material needs")
-                     19 (refresh! "black+friday" 11)
+                     19 (refresh! "robot+funny" 46)
                      22 (refresh! "black+friday" 12 "this song is called needings")
                      24 (refresh! "rickandmorty")
                      30 (refresh! "computer+art" 10 "by Gangol und mit")
                      32 (refresh! "computer+cat" 12)
                      34 (refresh! "robot+crying" nil "enjoy")
+                     37 (refresh! "robot+fail")
+                     41 (refresh! "robot+fail" 2)
+                     45 (refresh! "robot+fail" 4)
                      50 (refresh! "robot+crying" 4 "")
+                     52 (refresh! "cat+robot" 21)
                      62 (refresh! "funny+robot" 10)
                      66 (refresh! "house+robot")
                      68 (refresh! "dog+funny")
                      72 (refresh! "kite")
                      78 (refresh! "funny+robot" 2)
                      86 (refresh! "computer+fail" 5)        ;; upgrade
+                     ;; 90 (refresh! "funny+home" 49)
+                     92 (refresh! "funny+home" 49)
                      95 (refresh! "funny+dog" 3)
                      99 (refresh! "cat+robot")
+                     105 (refresh! "car+robot" 5)
+                     107 (refresh! "god+robot" 1)
+                     108 (refresh! "job+robot")
                      113 (refresh! "computer+fail" 3)       ;; upgrade
-                     118 (refresh! "robot+home" 2)          ;; help
+                     118 (refresh! "help+robot" 21)         ;; help
                      126 (refresh! "robot+good" 3)
-                     156 (refresh! "comgputer+ram")         ;; upgrade
-                     163 (refresh! "computer+ram")          ;; home
+                     142 (refresh! "messy+house" 5)
+                     147 (refresh! "robot+car" 6)
+                     153 (refresh! "god+robot" 12)
+                     156 (refresh! "computer+ram")          ;; upgrade
+                     164 (refresh! "god")
+                     168 (refresh! "job+robot" 12)
                      182 (refresh! "matrix+funny" 1)
                      200 (refresh! "matrix+funny" 2)
                      "default")))
@@ -68,7 +78,7 @@
 (defn count-up []
       (let [mm (str (.floor js/Math (/ @duration 60)) ":" (mod @duration 60))]
            [:div.count-up
-            [:div.clock @duration]]))
+            [:div @duration]]))
 
 (defn gif-comp []
       [:div.row.gifComp
@@ -102,16 +112,13 @@
          [b]]]])
 
 (defn main-component []
-      (let [_ (refresh! "nyan+cat+robot")]
+      (let []
            (r/create-class
-             {:component-did-mount
-              (fn [])
-              :reagent-render
-              (fn []
-                  [:div.container-fluid
-                   [gif-comp]
-                   [audio-comp]
-                   [text-comp count-up play-btn]])})))
+             {:component-did-mount #(refresh! "nyan+cat+robot")
+              :reagent-render      (fn [] [:div.container-fluid
+                                           [gif-comp]
+                                           [audio-comp]
+                                           [text-comp count-up play-btn]])})))
 
 (defn init []
       (r/render-component [main-component]
