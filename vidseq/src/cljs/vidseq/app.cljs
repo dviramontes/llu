4c5ed9d54ff93ffebaca0cb91caf4c3a08d05bc3
dviramontes <dviramontes@gmail.com>
2016-02-19 21:35:38 -0700

    adds needings audio as .wav, adds play btn

--- vidseq/src/cljs/vidseq/app.cljs
+++ vidseq/src/cljs/vidseq/app.cljs
@@ -5,25 +5,28 @@
 
 (enable-console-print!)
 
-(defonce duration (r/atom 0))
+(def duration (r/atom 0))
 
 (defonce end 201)
 
 (defonce time-color (r/atom "#f34"))
 
-(defonce time-updater! (js/setInterval #(swap! duration inc) 1000))
+(declare time-updater!)
 
 (defonce source-url (r/atom {:query  ""
                              :offset 0}))
+
 
 (defn refresh! [query offset]
       (reset! source-url {:query  query
                           :offset offset}))
 
 (add-watch duration :duration-watcher
-           (fn [_ _ _ t]
+           (fn [_ _ ot nt]
                ;; (str (.floor js/Math (/ t 60)) (mod t 60))
-               (print t)))
+               (if-not (nil? nt)
+                 (print (str (.floor js/Math (/ nt 60)) (mod nt 60))))))
+
 
 (defn count-up []
       (let [mm (str (.floor js/Math (/ @duration 60)) ":" (mod @duration 60))]
@@ -38,17 +41,36 @@
                  :loop     true
                  :src      @source-url}]]])
 
-(defn text-comp [c]
+(defn audio-comp []
+      [:div.row.audio-tag
+       [:div.col-lg-12
+        [:audio {:src "media/needings.wav"
+                 :id "audio-el"
+                 :controls "controls"
+                 :autoPlay false}]]])
+
+(defn play-btn []
+      [:button.btn.btn-danger
+       {:on-click (fn []
+                      (.play (.querySelector js/document "#audio-el"))
+                      (js/setInterval #(swap! duration inc) 1000))}
+       "play"])
+
+(defn text-comp [c b]
       [:div.row.text-comp
        [:div.col-lg-12.jumbotron
         [:p.text-center
          [:strong "mdklsamd"]]
-        [c {:class "align-right"}]]])
+        [c {:class "align-right"}]
+        [:p.pull-right
+         [b]]]])
 
 (defn main-component []
-      [:div.container-fluid
-       [gif-comp]
-       [text-comp count-up]])
+      (let []
+           [:div.container-fluid
+            [gif-comp]
+            [audio-comp]
+            [text-comp count-up play-btn]]))
 
 (defn init []
       (r/render-component [main-component]
