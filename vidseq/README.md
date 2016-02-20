b93ac330c0628685ed086c10c508ba9bfe4e8145
dviramontes <dviramontes@gmail.com>
2016-02-20 12:52:16 -0700

    adds readme and more keyframes

---
+++ vidseq/README.md
@@ -0,0 +1,18 @@
+## Development:
+
+- run $ boot dev
+- visit localhost:3000
+
+## Browser Connected REPL:
+
+- boot dev
+- boot repl --client
+- (start-repl)
+
+## Building Static Assets for Deployment:
+
+- boot production build target
+
+## Testing:
+
+- boot test
--- vidseq/src/cljs/vidseq/app.cljs
+++ vidseq/src/cljs/vidseq/app.cljs
@@ -1,38 +1,74 @@
 (ns vidseq.app
   (:require [reagent.core :as r]
-            [ajax.core :refer [GET]]
-            [cljsjs.moment :as moment]))
+            [ajax.core :refer [GET]]))
 
 (enable-console-print!)
 
 (def duration (r/atom 0))
 
-(defonce end 201)
+(def query (r/atom {:query  ""
+                    :offset 0}))
 
-(defonce time-color (r/atom "#f34"))
+(def source-url (r/atom ""))
 
-(declare time-updater!)
+(def voice-1 (r/atom "My first show show"))
 
-(defonce source-url (r/atom {:query  ""
-                             :offset 0}))
+(defn get-random-int [min max]
+      (.floor js/Math (+ min (* (- max min) (.random js/Math)))))
 
 
-(defn refresh! [query offset]
-      (reset! source-url {:query  query
-                          :offset offset}))
+(defn refresh! [q & [offset txt]]
+      ;; http://api.giphy.com/v1/gifs/search?q=cat+funny&api_key=dc6zaTOxFJmzC&limit=1&offset=2
+      (let [off (get-random-int 1 12)]
+           (if txt (reset! voice-1 txt))
+           (GET "http://api.giphy.com/v1/gifs/search"
+                {:params          {:q       q
+                                   :api_key "dc6zaTOxFJmzC"
+                                   :limit   1
+                                   :offset  (or offset 0)}
+                 :handler         (fn [res]
+                                      (reset! source-url
+                                              (:mp4 (:looping (:images (first (:data res)))))))
+                 :response-format :json
+                 :keywords?       true})))
 
 (add-watch duration :duration-watcher
-           (fn [_ _ ot nt]
-               ;; (str (.floor js/Math (/ t 60)) (mod t 60))
-               (if-not (nil? nt)
-                 (print (str (.floor js/Math (/ nt 60)) (mod nt 60))))))
+           (fn [_ _ _ t]
+               (case t
+                     2 (refresh! "nyan+cat+robot")
+                     4 (refresh! "funny+cat" 11 "this is the story")
+                     8 (refresh! "robot+love" 5 "of a robot")
+                     12 (refresh! "robot+walking" nil "who falls in love")
+                     16 (refresh! "black+friday" 1 "with material needs")
+                     19 (refresh! "black+friday" 11)
+                     22 (refresh! "black+friday" 12 "this song is called needings")
+                     24 (refresh! "rickandmorty")
+                     30 (refresh! "computer+art" 10 "by Gangol und mit")
+                     32 (refresh! "computer+cat" 12)
+                     34 (refresh! "robot+crying" nil "enjoy")
+                     50 (refresh! "robot+crying" 4 "")
+                     62 (refresh! "funny+robot" 10)
+                     66 (refresh! "house+robot")
+                     68 (refresh! "dog+funny")
+                     72 (refresh! "kite")
+                     78 (refresh! "funny+robot" 2)
+                     86 (refresh! "computer+fail" 5)        ;; upgrade
+                     95 (refresh! "funny+dog" 3)
+                     99 (refresh! "cat+robot")
+                     113 (refresh! "computer+fail" 3)       ;; upgrade
+                     118 (refresh! "robot+home" 2)          ;; help
+                     126 (refresh! "robot+good" 3)
+                     156 (refresh! "comgputer+ram")         ;; upgrade
+                     163 (refresh! "computer+ram")          ;; home
+                     182 (refresh! "matrix+funny" 1)
+                     200 (refresh! "matrix+funny" 2)
+                     "default")))
 
 
 (defn count-up []
       (let [mm (str (.floor js/Math (/ @duration 60)) ":" (mod @duration 60))]
            [:div.count-up
-            {:style {:color @time-color}}
-            [:div.clock mm]]))
+            [:div.clock @duration]]))
 
 (defn gif-comp []
       [:div.row.gifComp
@@ -44,9 +80,9 @@
 (defn audio-comp []
       [:div.row.audio-tag
        [:div.col-lg-12
-        [:audio {:src "media/needings.wav"
-                 :id "audio-el"
-                 :controls "controls"
+        [:audio {:src      "media/needings.wav"
+                 :id       "audio-el"
+                 ;; :controls "controls"
                  :autoPlay false}]]])
 
 (defn play-btn []
@@ -59,18 +95,23 @@
 (defn text-comp [c b]
       [:div.row.text-comp
        [:div.col-lg-12.jumbotron
-        [:p.text-center
-         [:strong "mdklsamd"]]
+        [:p.text-center.voices
+         [:em @voice-1]]
         [c {:class "align-right"}]
         [:p.pull-right
          [b]]]])
 
 (defn main-component []
-      (let []
-           [:div.container-fluid
-            [gif-comp]
-            [audio-comp]
-            [text-comp count-up play-btn]]))
+      (let [_ (refresh! "nyan+cat+robot")]
+           (r/create-class
+             {:component-did-mount
+              (fn [])
+              :reagent-render
+              (fn []
+                  [:div.container-fluid
+                   [gif-comp]
+                   [audio-comp]
+                   [text-comp count-up play-btn]])})))
 
 (defn init []
       (r/render-component [main-component]
